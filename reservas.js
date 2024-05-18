29a14db02a0ac8b5b923df0525eaa7e4975a9177
mgiorgi32 <marcosgiorgi2004@gmail.com>
2024-05-17 23:22:43 -0300

    first commit

---
+++ reservas.js
@@ -0,0 +1,13 @@
+function mailto(){
+    var nombre = document.getElementById('validationTooltip01').value;
+    var apellido = document.getElementById('validationTooltip02').value;
+    var email = document.getElementById('inputEmail4').value;
+    var destino = document.getElementById('validationTooltip04').value;
+    var presupuesto = document.getElementById('validationTooltip05').value;
+    var cuerpoCorreo = "Nombre: " + nombre + "\n";
+    cuerpoCorreo += "Apellido: " + apellido + "\n";
+    cuerpoCorreo += "Email: " + email + "\n";
+    cuerpoCorreo += "Destino: " + destino + "\n";
+    cuerpoCorreo += "Presupuesto: $" + presupuesto + "\n";
+    window.location.href = "mailto:codeninjas827@gmali.com?subject=Solicitud de Viaje&body=" + encodeURIComponent(cuerpoCorreo);
+}
